0658b9b2f87a3e0138287c7d6d021c63dfd04631
Lachlan van der Velden <me@lachlanvelden.com>
2019-03-15 18:09:09 +1100

    Add volume tracker console app

---
+++ VolumeTracker/Program.cs
@@ -0,0 +1,46 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeTracker
+{
+    class Program : IObserver<DeviceVolumeChangedArgs>
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+
+        }
+
+        void IObserver<DeviceVolumeChangedArgs>.OnCompleted()
+        {
+            Console.WriteLine("Completed");
+        }
+
+        void IObserver<DeviceVolumeChangedArgs>.OnError(Exception error)
+        {
+            Console.WriteLine(error);
+        }
+
+        void IObserver<DeviceVolumeChangedArgs>.OnNext(DeviceVolumeChangedArgs value)
+        {
+            Console.WriteLine(value.Volume);
+        }
+
+        private void Start()
+        {
+
+            var controller = new CoreAudioController(true, false, false, false);
+            CoreAudioDevice defaultPlaybackDevice = controller.DefaultPlaybackDevice;
+            
+            defaultPlaybackDevice.ReloadAudioEndpointVolume();
+            defaultPlaybackDevice.VolumeChanged.Subscribe(this);
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
